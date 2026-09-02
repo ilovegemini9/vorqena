aafd4138a9a334d4bfec4f90e0a1b769c1a31747
ilovegemini9 <tikfyp60@gmail.com>
2026-09-02 08:09:14 +0100

    feat: rebuild Vorqena app shell from scratch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
 import { Route, Switch } from "wouter";
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import IntentPage from "./pages/IntentPage";
-import { AgeCalculatorPage, BmiCalculatorPage, MortgageCalculatorPage } from "./pages/CalculatorPages";
-import PublicCalculatorPage from "./pages/PublicCalculatorPage";
 import NotFound from "./pages/NotFound";
+import "./index.css";
 
 function Router() {
   return (
@@ -18,25 +13,11 @@ function Router() {
       <Route path="/decide" component={() => <IntentPage type="decide" />} />
       <Route path="/when" component={() => <IntentPage type="when" />} />
       <Route path="/cost" component={() => <IntentPage type="cost" />} />
-      <Route path="/mortgage" component={MortgageCalculatorPage} />
-      <Route path="/bmi" component={BmiCalculatorPage} />
-      <Route path="/age" component={AgeCalculatorPage} />
-      <Route path="/404" component={NotFound} />
-      <Route path="/:slug" component={PublicCalculatorPage} />
       <Route component={NotFound} />
     </Switch>
   );
 }
 
 export default function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+  return <Router />;
 }
