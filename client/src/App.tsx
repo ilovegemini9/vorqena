93d3b78935d2b87c5dc8f22d70fa3809652cdca2
ilovegemini9 <tikfyp60@gmail.com>
2026-09-02 08:16:16 +0100

    feat: wire Vorqena search and tool routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,21 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
 import IntentPage from "./pages/IntentPage";
+import SearchPage from "./pages/SearchPage";
+import ToolPage from "./pages/ToolPage";
 import NotFound from "./pages/NotFound";
 import "./index.css";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/fix" component={() => <IntentPage type="fix" />} />
-      <Route path="/calculate" component={() => <IntentPage type="calculate" />} />
-      <Route path="/decide" component={() => <IntentPage type="decide" />} />
-      <Route path="/when" component={() => <IntentPage type="when" />} />
-      <Route path="/cost" component={() => <IntentPage type="cost" />} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 export default function App() {
-  return <Router />;
+  return <Switch>
+    <Route path="/" component={Home} />
+    <Route path="/search" component={SearchPage} />
+    <Route path="/tool/:slug" component={ToolPage} />
+    <Route path="/fix" component={() => <IntentPage type="fix" />} />
+    <Route path="/calculate" component={() => <IntentPage type="calculate" />} />
+    <Route path="/decide" component={() => <IntentPage type="decide" />} />
+    <Route path="/when" component={() => <IntentPage type="when" />} />
+    <Route path="/cost" component={() => <IntentPage type="cost" />} />
+    <Route component={NotFound} />
+  </Switch>;
 }
